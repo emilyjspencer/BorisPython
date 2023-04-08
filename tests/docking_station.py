cd39804848ce4490a8a885219c24827a91dad7f9
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-04-08 20:08:08 +0100

    Adds method and test to dock bike

--- tests/docking_station.py
+++ tests/docking_station.py
@@ -1,7 +1,15 @@
+#from . import Bike
+
 class DockingStation:
+
 
     def __init__(self, location):
         self.location = location
+        self.bikes = []
 
     def releaseBike(self):
         return "Bike"
+
+    def dock(self, bike):
+        self.bikes.append(bike)
+        return self.bikes
--- tests/docking_station_test.py
+++ tests/docking_station_test.py
@@ -1,10 +1,16 @@
 import pytest
 from . import DockingStation
+from . import Bike
 
 dockingStation = DockingStation('Bank')
+bike = Bike()
 
 def test_docking_station_releaseBike():
     assert dockingStation.releaseBike() == "Bike"
 
-
+def test_bike_can_be_added_to_docking_station():
+     
+    dockingStation.dock(bike)
+    assert dockingStation.bikes == [bike]
+    
 
